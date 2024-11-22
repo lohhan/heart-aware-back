62e5b0435ed6624177633dcd104db4d035ad9851
Lohhan <lohhang05@gmail.com>
2024-11-22 20:03:57 -0300

    Ajustando o Deploy

--- core/settings.py
+++ core/settings.py
@@ -16,9 +16,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+PORT = os.environ.get("PORT", "8000")  # 8000 é o padrão
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
